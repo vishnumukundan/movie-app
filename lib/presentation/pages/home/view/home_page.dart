cb457c27da1b7c343b3268ceee1e48203496830b
Vishnu Mukundan <vishnumukundan040@gmail.com>
2022-08-30 00:36:25 +0530

    Custom widget builder created,

--- lib/presentation/pages/home/view/home_page.dart
+++ lib/presentation/pages/home/view/home_page.dart
@@ -6,19 +6,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movie_app/core/services/sort_list_by.dart';
 import 'package:movie_app/core/utils/generics/app_route/custom_scroll_behavior.dart';
+import 'package:movie_app/core/utils/generics/custom_widget_builder.dart';
 import 'package:movie_app/data/sources/dummy/dummy_data.dart';
 import 'package:movie_app/presentation/bloc/home/image_slider/image_slider_cubit.dart';
 import 'package:movie_app/presentation/components/movie_list_scroll.dart';
 import 'package:movie_app/presentation/pages/home/widgets/genres_scroll.dart';
+import 'package:movie_app/presentation/pages/home/widgets/latest_trailers.dart';
 import 'package:movie_app/presentation/pages/home/widgets/upcoming_movies/upcoming_stack.dart';
 import 'package:movie_app/presentation/themes/colors.dart';
 
 import '../../../components/text.dart';
 import '../../../components/background.dart';
+import '../../../components/video_card_item.dart';
 import '../../../themes/screen_size_config.dart';
 import '../../../themes/values.dart';
 import '../../main/widgets/appbar.dart';
 import '../widgets/image_slider.dart';
+
+final _data = moviePosterDummyData;
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
@@ -54,6 +59,7 @@ class HomePage extends StatelessWidget {
                     dataList: moviePosterDummyData,
                     buttonVisibility: true,
                   ),
+                  const LatestTrailers__widget(),
                   MovieListScroll__widget(
                       title: 'Top 10 Action Movies',
                       dataList: moviePosterDummyData),
@@ -63,6 +69,21 @@ class HomePage extends StatelessWidget {
                   MovieListScroll__widget(
                       title: 'Top 10 Drama Movies',
                       dataList: moviePosterDummyData),
+                  CustomWidgetBuilder(
+                    spacing: 16.0,
+                    itemCount: 3,
+                    scrollDirection: Axis.horizontal,
+                    builder: (context, index) {
+                      return Container(
+                        height: 150,
+                        width: 250,
+                        color: kColorWhite50,
+                        child: Center(
+                          child: Bold__text(text: '$index', fontSize: 16.0),
+                        ),
+                      );
+                    },
+                  ),
                 ],
               ),
             ),
